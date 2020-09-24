5b34616003d3cba727beabd96afb3505672ef6d2
Sean Leffler <sean@errno.com>
2020-09-24 10:14:53 -0700

    hecs smart pointer changes

--- src/ecs.rs
+++ src/ecs.rs
@@ -1,21 +1,14 @@
 use {
     crossbeam_channel::Sender,
     hashbrown::HashMap,
-    hecs,
+    hecs::{self, *},
     hibitset::{AtomicBitSet, BitSetLike},
-    std::{any::TypeId, ops, vec::Drain},
+    std::{any::TypeId, vec::Drain},
 };
 
 pub mod hierarchy;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct Entity(hecs::Entity);
-
-impl Entity {
-    pub fn id(&self) -> u32 {
-        self.0.id()
-    }
-}
+pub use hecs::Entity;
 
 #[derive(Debug, Clone, Copy)]
 pub enum Event {
@@ -32,88 +25,6 @@ impl EventSender for Sender<Event> {
     fn send_event(&self, event: Event) -> bool {
         self.try_send(event).is_ok()
     }
-}
-
-pub enum Normal {}
-
-pub enum Flagged {}
-
-pub trait WrappedMut<'q, T, U>
-where
-    T: ?Sized + Component,
-    U: ops::DerefMut<Target = T> + 'q,
-{
-    const IS_FLAGGED: bool;
-    type Output;
-
-    fn transform(this: U, id: u32, flags: &'q HashMap<TypeId, AtomicBitSet>) -> Self::Output;
-}
-
-pub struct FlaggedRefMut<'a, T, U>
-where
-    T: ?Sized + Component,
-    U: ops::DerefMut<Target = T> + 'a,
-{
-    id: u32,
-    flags: &'a HashMap<TypeId, AtomicBitSet>,
-    inner: U,
-}
-
-impl<'a, T, U> ops::Deref for FlaggedRefMut<'a, T, U>
-where
-    T: ?Sized + Component,
-    U: ops::DerefMut<Target = T> + 'a,
-{
-    type Target = T;
-
-    fn deref(&self) -> &Self::Target {
-        &*self.inner
-    }
-}
-
-impl<'a, T, U> ops::DerefMut for FlaggedRefMut<'a, T, U>
-where
-    T: ?Sized + Component,
-    U: ops::DerefMut<Target = T> + 'a,
-{
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        self.flags[&TypeId::of::<T>()].add_atomic(self.id);
-        &mut *self.inner
-    }
-}
-
-impl<'a, T, U> WrappedMut<'a, T, U> for Flagged
-where
-    T: ?Sized + Component,
-    U: ops::DerefMut<Target = T> + 'a,
-{
-    const IS_FLAGGED: bool = true;
-    type Output = FlaggedRefMut<'a, T, U>;
-
-    fn transform(this: U, id: u32, flags: &'a HashMap<TypeId, AtomicBitSet>) -> Self::Output {
-        FlaggedRefMut {
-            flags,
-            id,
-            inner: this,
-        }
-    }
-}
-
-impl<'a, T, U> WrappedMut<'a, T, U> for Normal
-where
-    T: ?Sized + Component,
-    U: ops::DerefMut<Target = T> + 'a,
-{
-    const IS_FLAGGED: bool = false;
-    type Output = U;
-
-    fn transform(this: U, _id: u32, _flags: &'a HashMap<TypeId, AtomicBitSet>) -> Self::Output {
-        this
-    }
-}
-
-pub trait Component: hecs::Component {
-    type Kind: for<'a> WrappedMut<'a, Self, &'a mut Self>;
 }
 
 pub trait DynamicBundle: Into<<Self as DynamicBundle>::Hecs> {
@@ -160,9 +71,9 @@ impl EntityBuilder {
     pub fn add<T: Component>(&mut self, component: T) -> &mut Self {
         self.builder.add(component);
 
-        if T::Kind::IS_FLAGGED {
-            self.flagged_types.push(TypeId::of::<T>());
-        }
+        // if T::Kind::IS_FLAGGED {
+        //     self.flagged_types.push(TypeId::of::<T>());
+        // }
 
         self
     }
@@ -185,6 +96,8 @@ pub struct World {
     channels: HashMap<TypeId, Vec<Box<dyn EventSender>>>,
 }
 
+pub type Flags = HashMap<TypeId, AtomicBitSet>;
+
 impl World {
     pub fn new() -> Self {
         Self {
@@ -201,44 +114,47 @@ impl World {
         for typeid in self.ecs.entity(e).expect("just created").component_types() {
             if let Some(channel) = self.channels.get(&typeid) {
                 for subscriber in channel {
-                    subscriber.send_event(Event::Created(Entity(e)));
+                    subscriber.send_event(Event::Created(e));
                 }
             }
         }
 
-        Entity(e)
+        e
     }
 
-    pub fn query<Q: hecs::Query>(&self) -> QueryBorrow<Q> {
-        QueryBorrow {
-            flags: &self.flags,
-            borrow: self.ecs.query(),
-        }
+    pub fn contains(&self, entity: Entity) -> bool {
+        self.ecs.contains(entity)
     }
 
-    pub fn query_one<Q: hecs::Query>(
-        &self,
-        entity: Entity,
-    ) -> Result<QueryOne<Q>, hecs::NoSuchEntity> {
-        Ok(QueryOne {
-            id: entity.id(),
-            flags: &self.flags,
-            borrow: self.ecs.query_one(entity.0)?,
-        })
-    }
-
-    pub fn get<C: Component>(&self, entity: Entity) -> Result<hecs::Ref<C>, hecs::ComponentError> {
-        self.ecs.get(entity.0)
-    }
-
-    pub fn get_mut<'a, C, R>(&'a self, entity: Entity) -> Result<R, hecs::ComponentError>
+    pub fn query<'w, Q>(&'w self) -> hecs::QueryBorrow<'w, Q, &'w Flags>
     where
-        C: Component,
-        C::Kind: WrappedMut<'a, C, hecs::RefMut<'a, C>, Output = R>,
+        Q: hecs::Query<'w, &'w Flags>,
     {
-        self.ecs
-            .get_mut::<C>(entity.0)
-            .map(|p| C::Kind::transform(p, entity.id(), &self.flags))
+        self.ecs.query_with_context(&self.flags)
+    }
+
+    pub fn query_one<'w, Q>(
+        &'w self,
+        entity: Entity,
+    ) -> Result<hecs::QueryOne<'w, Q, &'w Flags>, hecs::NoSuchEntity>
+    where
+        Q: hecs::Query<'w, &'w Flags>,
+    {
+        self.ecs.query_one_with_context(entity, &self.flags)
+    }
+
+    pub fn get<'w, C: SmartComponent<&'w Flags>>(
+        &'w self,
+        entity: Entity,
+    ) -> Result<hecs::Ref<'w, C, &'w Flags>, hecs::ComponentError> {
+        self.ecs.get_with_context(entity, &self.flags)
+    }
+
+    pub fn get_mut<'w, C: SmartComponent<&'w Flags>>(
+        &'w self,
+        entity: Entity,
+    ) -> Result<hecs::RefMut<'w, C, &'w Flags>, hecs::ComponentError> {
+        self.ecs.get_mut_with_context(entity, &self.flags)
     }
 
     pub fn subscribe<T: Component>(&mut self, sender: Box<dyn EventSender>) {
@@ -255,7 +171,7 @@ impl World {
                     for id in set.iter() {
                         if let Some(e) = unsafe { self.ecs.resolve_unknown_gen(id) } {
                             for subscriber in channels {
-                                subscriber.send_event(Event::Modified(Entity(e)));
+                                subscriber.send_event(Event::Modified(e));
                             }
                         }
                     }
@@ -267,147 +183,14 @@ impl World {
     }
 }
 
-pub struct QueryBorrow<'w, Q: hecs::Query> {
-    flags: &'w HashMap<TypeId, AtomicBitSet>,
-    borrow: hecs::QueryBorrow<'w, Q>,
-}
-
-impl<'w, Q: hecs::Query> QueryBorrow<'w, Q> {
-    pub fn iter<'q>(&'q mut self) -> QueryIter<'q, 'w, Q> {
-        QueryIter {
-            flags: self.flags,
-            iter: self.borrow.iter(),
-        }
-    }
-
-    pub fn with<T: Component>(self) -> QueryBorrow<'w, hecs::With<T, Q>> {
-        QueryBorrow {
-            flags: self.flags,
-            borrow: self.borrow.with::<T>(),
-        }
-    }
-
-    pub fn without<T: Component>(self) -> QueryBorrow<'w, hecs::Without<T, Q>> {
-        QueryBorrow {
-            flags: self.flags,
-            borrow: self.borrow.without::<T>(),
-        }
-    }
-}
-
-pub struct QueryIter<'q, 'w, Q: hecs::Query> {
-    flags: &'w HashMap<TypeId, AtomicBitSet>,
-    iter: hecs::QueryIter<'q, 'w, Q>,
-}
-
-impl<'q, 'w, Q> Iterator for QueryIter<'q, 'w, Q>
-where
-    Q: hecs::Query,
-    <Q::Fetch as hecs::Fetch<'q>>::Item: TransformFetched<'q>,
-{
-    type Item = (
-        Entity,
-        <<Q::Fetch as hecs::Fetch<'q>>::Item as TransformFetched<'q>>::Output,
-    );
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.iter
-            .next()
-            .map(|(e, t)| (Entity(e), t.transform(e.id(), self.flags)))
-    }
-}
-
-pub struct QueryOne<'w, Q: hecs::Query> {
-    id: u32,
-    flags: &'w HashMap<TypeId, AtomicBitSet>,
-    borrow: hecs::QueryOne<'w, Q>,
-}
-
-impl<'w, Q: hecs::Query> QueryOne<'w, Q>
-where
-    Q: hecs::Query,
-    for<'q> <Q::Fetch as hecs::Fetch<'q>>::Item: TransformFetched<'q>,
-{
-    pub fn get(&mut self) -> Option<<<Q::Fetch as hecs::Fetch>::Item as TransformFetched>::Output> {
-        let Self { id, flags, borrow } = self;
-
-        borrow
-            .get()
-            .map(|fetched| TransformFetched::transform(fetched, *id, flags))
-    }
-
-    pub fn with<T: Component>(self) -> QueryOne<'w, hecs::With<T, Q>> {
-        QueryOne {
-            id: self.id,
-            flags: self.flags,
-            borrow: self.borrow.with(),
-        }
-    }
-
-    pub fn without<T: Component>(self) -> QueryOne<'w, hecs::Without<T, Q>> {
-        QueryOne {
-            id: self.id,
-            flags: self.flags,
-            borrow: self.borrow.without(),
-        }
-    }
-}
-
-pub trait TransformFetched<'q> {
-    type Output;
-
-    fn transform(self, id: u32, flags: &'q HashMap<TypeId, AtomicBitSet>) -> Self::Output;
-}
-
-impl<'q, T: Component> TransformFetched<'q> for &'q T {
-    type Output = &'q T;
-
-    fn transform(self, _id: u32, _flags: &'q HashMap<TypeId, AtomicBitSet>) -> Self::Output {
-        self
-    }
-}
-
-impl<'q, T: Component> TransformFetched<'q> for &'q mut T {
-    type Output = <T::Kind as WrappedMut<'q, T, &'q mut T>>::Output;
-
-    fn transform(self, id: u32, flags: &'q HashMap<TypeId, AtomicBitSet>) -> Self::Output {
-        <T::Kind as WrappedMut<'q, T, &'q mut T>>::transform(self, id, flags)
-    }
-}
-
-impl<'q, T: TransformFetched<'q>> TransformFetched<'q> for Option<T> {
-    type Output = Option<T::Output>;
-
-    fn transform(self, id: u32, flags: &'q HashMap<TypeId, AtomicBitSet>) -> Self::Output {
-        match self {
-            Some(t) => Some(t.transform(id, flags)),
-            None => None,
-        }
-    }
-}
-
 macro_rules! tuple_impl {
     ($($name: ident),*) => {
-        impl<'a, $($name: TransformFetched<'a>),*> TransformFetched<'a> for ($($name,)*) {
-            type Output = ($($name::Output,)*);
-
-            #[allow(unused_variables, non_snake_case)]
-            fn transform(self, id: u32, flags: &'a HashMap<TypeId, AtomicBitSet>) -> Self::Output {
-                let ($($name,)*) = self;
-                ($($name.transform(id, flags),)*)
-            }
-        }
-
-        impl<$($name: Component),*> Component for ($($name,)*) {
-            type Kind = Normal;
-        }
-
         impl<$($name: Component),*> DynamicBundle for ($($name,)*) {
             type Hecs = Self;
 
             #[allow(unused_variables)]
             fn init_flag_sets(&self, flags: &mut HashMap<TypeId, AtomicBitSet>) {
-                $(if $name::Kind::IS_FLAGGED { flags.entry(TypeId::of::<$name>()).or_default(); })*
+                $(flags.entry(TypeId::of::<$name>()).or_default();)*
             }
         }
     };
--- src/ecs/hierarchy.rs
+++ src/ecs/hierarchy.rs
@@ -1,25 +1,37 @@
 use {
     crossbeam_channel::Receiver,
     hashbrown::{HashMap, HashSet},
-    hibitset::BitSet,
+    hecs::SmartComponent,
+    hibitset::{BitSet, BitSetLike},
+    shrev::{EventChannel, ReaderId},
+    std::any::TypeId,
 };
 
-use crate::ecs::{Component, Entity, Event, Flagged, World};
+use crate::ecs::{Entity, Event, Flags, World};
+
+#[derive(Debug, Clone, Copy)]
+pub enum HierarchyEvent {
+    ModifiedOrCreated(Entity),
+    Removed(Entity),
+}
 
 #[derive(Debug, Clone, Copy)]
 pub struct Parent {
-    id: Entity,
+    parent_entity: Entity,
 }
 
-impl Component for Parent {
-    type Kind = Flagged;
+impl<'a> SmartComponent<&'a Flags> for Parent {
+    fn on_borrow_mut(&mut self, entity: hecs::Entity, context: &&'a Flags) {
+        context[&TypeId::of::<Self>()].add_atomic(entity.id());
+    }
 }
 
 pub struct Hierarchy {
     sorted: Vec<Entity>,
-    entities: HashMap<u32, Entity>,
+    entities: HashMap<u32, usize>,
 
-    parents: HashMap<Entity, Entity>,
+    roots: HashSet<Entity>,
+    current_parent: HashMap<Entity, Entity>,
     children: HashMap<Entity, Vec<Entity>>,
 
     created: BitSet,
@@ -29,6 +41,7 @@ pub struct Hierarchy {
     scratch_set: HashSet<Entity>,
 
     events: Receiver<Event>,
+    changed: EventChannel<HierarchyEvent>,
 }
 
 impl Hierarchy {
@@ -39,7 +52,8 @@ impl Hierarchy {
             sorted: Vec::new(),
             entities: HashMap::new(),
 
-            parents: HashMap::new(),
+            roots: HashSet::new(),
+            current_parent: HashMap::new(),
             children: HashMap::new(),
 
             created: BitSet::new(),
@@ -49,7 +63,63 @@ impl Hierarchy {
             scratch_set: HashSet::new(),
 
             events: receiver,
+            changed: EventChannel::new(),
         }
+    }
+
+    /// Get all entities that contain parents, in sorted order, where parents are guaranteed to
+    /// be before their children.
+    ///
+    /// Note: This does not include entities that **are** parents.
+    pub fn all(&self) -> &[Entity] {
+        self.sorted.as_slice()
+    }
+
+    /// Get the immediate children of a specific entity.
+    pub fn children(&self, entity: Entity) -> &[Entity] {
+        self.children
+            .get(&entity)
+            .map(|vec| vec.as_slice())
+            .unwrap_or(&[])
+    }
+
+    /// Get all children of this entity recursively as a `BitSet`
+    ///
+    /// This does not include the parent entity you pass in.
+    pub fn all_children(&self, entity: Entity) -> BitSet {
+        let mut entities = BitSet::new();
+        self.add_children_to_set(entity, &mut entities);
+        entities
+    }
+
+    fn add_children_to_set(&self, entity: Entity, set: &mut BitSet) {
+        if let Some(children) = self.children.get(&entity) {
+            for child in children {
+                set.add(child.id());
+                self.add_children_to_set(*child, set);
+            }
+        }
+    }
+
+    /// Returns an iterator over all of the recursive children of this entity.
+    ///
+    /// This does not include the parent entity you pass in. Parents are guaranteed to be
+    /// prior to their children.
+    pub fn all_children_iter(&self, entity: Entity) -> SubHierarchyIterator<'_> {
+        SubHierarchyIterator::new(self, entity)
+    }
+
+    /// Get the parent of a specific entity
+    pub fn parent(&self, entity: Entity) -> Option<Entity> {
+        self.current_parent.get(&entity).cloned()
+    }
+
+    pub fn track(&mut self) -> ReaderId<HierarchyEvent> {
+        self.changed.register_reader()
+    }
+
+    pub fn changed(&self) -> &EventChannel<HierarchyEvent> {
+        &self.changed
     }
 
     pub fn update(&mut self, world: &mut World) {
@@ -60,19 +130,291 @@ impl Hierarchy {
         for event in self.events.try_iter() {
             match event {
                 Event::Created(entity) => {
-                    self.entities.insert(entity.id(), entity);
                     self.created.add(entity.id());
                 }
                 Event::Modified(entity) => {
                     self.modified.add(entity.id());
                 }
                 Event::Destroyed(entity) => {
-                    self.entities.remove(&entity.id());
                     self.destroyed.add(entity.id());
                 }
             }
         }
 
         self.scratch_set.clear();
+
+        for id in (&self.destroyed).iter() {
+            if let Some(index) = self.entities.get(&id) {
+                self.scratch_set.insert(self.sorted[*index]);
+            }
+        }
+
+        for entity in &self.roots {
+            if !world.contains(*entity) {
+                self.scratch_set.insert(*entity);
+            }
+        }
+
+        if !self.scratch_set.is_empty() {
+            let mut i = 0;
+            let mut min_index = std::usize::MAX;
+            while i < self.sorted.len() {
+                let entity = self.sorted[i];
+                let remove = self.scratch_set.contains(&entity)
+                    || self
+                        .current_parent
+                        .get(&entity)
+                        .map(|parent_entity| self.scratch_set.contains(&parent_entity))
+                        .unwrap_or(false);
+
+                if remove {
+                    if i < min_index {
+                        min_index = i;
+                    }
+
+                    self.scratch_set.insert(entity);
+                    self.sorted.remove(i);
+
+                    if let Some(children) = self
+                        .current_parent
+                        .get(&entity)
+                        .cloned()
+                        .and_then(|parent_entity| self.children.get_mut(&parent_entity))
+                    {
+                        if let Some(pos) = children.iter().position(|e| *e == entity) {
+                            children.swap_remove(pos);
+                        }
+                    }
+
+                    self.current_parent.remove(&entity);
+                    self.children.remove(&entity);
+                    self.entities.remove(&entity.id());
+                } else {
+                    i += 1;
+                }
+            }
+            for i in min_index..self.sorted.len() {
+                self.entities.insert(self.sorted[i].id(), i);
+            }
+            for entity in &self.scratch_set {
+                // TODO
+                // self.changed.single_write(HierarchyEvent::Removed(*entity));
+                self.roots.remove(entity);
+            }
+        }
+
+        // insert new components in hierarchy
+        let created = &self.created;
+        self.scratch_set.clear();
+        for (entity, parent) in world
+            .query::<&Parent>()
+            .iter()
+            .filter(|(e, _)| created.contains(e.id()))
+        {
+            let parent_entity = parent.parent_entity;
+
+            // if we insert a parent component on an entity that have children, we need to make
+            // sure the parent is inserted before the children in the sorted list
+            let insert_index = self
+                .children
+                .get(&entity)
+                .and_then(|children| {
+                    children
+                        .iter()
+                        .map(|child_entity| self.entities.get(&child_entity.id()).unwrap())
+                        .min()
+                        .cloned()
+                })
+                .unwrap_or_else(|| self.sorted.len());
+
+            self.entities.insert(entity.id(), insert_index);
+
+            if insert_index >= self.sorted.len() {
+                self.sorted.push(entity);
+            } else {
+                self.sorted.insert(insert_index, entity);
+                for i in insert_index..self.sorted.len() {
+                    self.entities.insert(self.sorted[i].id(), i);
+                }
+            }
+
+            {
+                let children = self.children.entry(parent_entity).or_default();
+
+                children.push(entity);
+            }
+
+            self.current_parent.insert(entity, parent_entity);
+            self.scratch_set.insert(entity);
+            if !self.current_parent.contains_key(&parent_entity) {
+                self.roots.insert(parent_entity);
+            }
+            self.roots.remove(&entity);
+        }
+
+        let modified = &self.modified;
+        for (entity, parent) in world
+            .query::<&Parent>()
+            .iter()
+            .filter(|(e, _)| modified.contains(e.id()))
+        {
+            let parent_entity = parent.parent_entity;
+            // if theres an old parent
+            if let Some(old_parent) = self.current_parent.get(&entity).cloned() {
+                // if the parent entity was not changed, ignore event
+                if old_parent == parent_entity {
+                    continue;
+                }
+                // remove entity from old parents children
+                if let Some(children) = self.children.get_mut(&old_parent) {
+                    if let Some(pos) = children.iter().position(|e| *e == entity) {
+                        children.remove(pos);
+                    }
+                }
+            }
+
+            // insert in new parents children
+            self.children
+                .entry(parent_entity)
+                .or_insert_with(Vec::default)
+                .push(entity);
+
+            // move entity in sorted if needed
+            let entity_index = self.entities.get(&entity.id()).cloned().unwrap();
+            if let Some(parent_index) = self.entities.get(&parent_entity.id()).cloned() {
+                let mut offset = 0;
+                let mut process_index = parent_index;
+                while process_index > entity_index {
+                    let move_entity = self.sorted.remove(process_index);
+                    self.sorted.insert(entity_index, move_entity);
+                    offset += 1;
+                    process_index = self
+                        .current_parent
+                        .get(&move_entity)
+                        .and_then(|p_entity| self.entities.get(&p_entity.id()))
+                        .map(|p_index| p_index + offset)
+                        .unwrap_or(0);
+                }
+
+                // fix indexes
+                if parent_index > entity_index {
+                    for i in entity_index..parent_index {
+                        self.entities.insert(self.sorted[i].id(), i);
+                    }
+                }
+            }
+
+            self.current_parent.insert(entity, parent_entity);
+            self.scratch_set.insert(entity);
+
+            if !self.current_parent.contains_key(&parent_entity) {
+                self.roots.insert(parent_entity);
+            }
+        }
+
+        if !self.scratch_set.is_empty() {
+            for i in 0..self.sorted.len() {
+                let entity = self.sorted[i];
+                let notify = self.scratch_set.contains(&entity)
+                    || self
+                        .current_parent
+                        .get(&entity)
+                        .map(|parent_entity| self.scratch_set.contains(&parent_entity))
+                        .unwrap_or(false);
+                if notify {
+                    self.scratch_set.insert(entity);
+                    //self.changed.single_write(HierarchyEvent::Modified(entity));
+                }
+            }
+        }
+
+        self.scratch_set.clear();
+        for entity in &self.roots {
+            if !self.children.contains_key(entity) {
+                self.scratch_set.insert(*entity);
+            }
+        }
+
+        for entity in &self.scratch_set {
+            self.roots.remove(entity);
+        }
+    }
+}
+
+pub struct SubHierarchyIterator<'a> {
+    current_index: usize,
+    end_index: usize,
+    hierarchy: &'a Hierarchy,
+    entities: BitSet,
+}
+
+impl<'a> SubHierarchyIterator<'a> {
+    fn new(hierarchy: &'a Hierarchy, root: Entity) -> Self {
+        let max = hierarchy.sorted.len();
+        let root_index = hierarchy
+            .children
+            .get(&root)
+            .map(|children| {
+                children
+                    .iter()
+                    .map(|c| hierarchy.entities.get(&c.id()).cloned().unwrap_or(max))
+                    .min()
+                    .unwrap_or(max)
+            })
+            .unwrap_or(max);
+        let mut iter = SubHierarchyIterator {
+            hierarchy,
+            current_index: root_index,
+            end_index: 0,
+            entities: BitSet::new(),
+        };
+        iter.process_entity(root);
+        if root_index != max {
+            iter.process_entity(hierarchy.sorted[root_index]);
+        }
+        iter
+    }
+
+    fn process_entity(&mut self, child: Entity) {
+        if let Some(children) = self.hierarchy.children.get(&child) {
+            for child in children {
+                self.entities.add(child.id());
+                if let Some(index) = self.hierarchy.entities.get(&child.id()) {
+                    if *index > self.end_index {
+                        self.end_index = *index;
+                    }
+                }
+            }
+        }
+    }
+}
+
+impl<'a> Iterator for SubHierarchyIterator<'a> {
+    type Item = Entity;
+
+    fn next(&mut self) -> Option<<Self as Iterator>::Item> {
+        if self.current_index >= self.hierarchy.sorted.len() || self.current_index > self.end_index
+        {
+            None
+        } else {
+            let entity = self.hierarchy.sorted[self.current_index];
+            let mut found_next = false;
+            while !found_next {
+                self.current_index += 1;
+                if self.current_index > self.end_index
+                    || self.current_index >= self.hierarchy.sorted.len()
+                {
+                    found_next = true;
+                } else if self
+                    .entities
+                    .contains(self.hierarchy.sorted[self.current_index].id())
+                {
+                    found_next = true;
+                    let current_index = self.current_index; // compiler fails to realise a usize is Copy, so we break it out
+                    self.process_entity(self.hierarchy.sorted[current_index]);
+                }
+            }
+            Some(entity)
+        }
     }
 }
