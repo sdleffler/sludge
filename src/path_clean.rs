43bcc4540f92dfbb5648f844891d26010fb6115e
Sean Leffler <sean@errno.com>
2020-11-29 13:34:59 -0800

    [sludge] Clean up docs and add proper licensing info for scene.rs

--- src/path_clean.rs
+++ src/path_clean.rs
@@ -1,5 +1,7 @@
 //! `path-clean` is a Rust port of the the `cleanname` procedure from the Plan 9 C library, and is similar to
-//! [`path.Clean`](https://golang.org/pkg/path/#Clean) from the Go standard library. It works as follows:
+//! [`path.Clean`](https://golang.org/pkg/path/#Clean) from the Go standard library.
+//!
+//! It works as follows:
 //!
 //! 1. Reduce multiple slashes to a single slash.
 //! 2. Eliminate `.` path name elements (the current directory).
@@ -23,6 +25,28 @@
 //!     PathBuf::from("/path")
 //! );
 //! ```
+
+/*
+ * Copyright (c) 2018 Dan Reeves
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 use std::path::{Path, PathBuf};
 
--- src/scene.rs
+++ src/scene.rs
@@ -1,3 +1,6 @@
+//! This is a modified version of the `scene` module from `ggez-goodies`. Please
+//! see the licensing information below in the source file.
+//!
 //! The Scene system is basically for transitioning between
 //! *completely* different states that have entirely different game
 //! loops and but which all share a state.  It operates as a stack, with new
@@ -13,6 +16,30 @@
 //! This is basically identical in concept to the Amethyst engine's scene
 //! system, the only difference is the details of how the pieces are put
 //! together.
+
+/*
+ * MIT License
+ *
+ * Copyright (c) 2016-2018 the ggez developers
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 use anyhow::Result;
 
