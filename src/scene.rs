15401242123e4ff0fea82c3d8715062086ecd79c
Sean Leffler <sean@errno.com>
2020-12-01 17:42:26 -0800

    [sludge] interior mutability of scenes, scenestack mechanism changes

--- src/scene.rs
+++ src/scene.rs
@@ -41,26 +41,78 @@
  * SOFTWARE.
  */
 
-use anyhow::Result;
+use {
+    anyhow::*,
+    atomic_refcell::AtomicRefCell,
+    std::{borrow::Cow, fmt, sync::Arc},
+};
 
-/// A command to change to a new scene, either by pushign a new one,
-/// popping one or replacing the current scene (pop and then push).
-pub enum SceneSwitch<C, Ev> {
-    None,
-    Push(Box<dyn Scene<C, Ev>>),
-    Replace(Box<dyn Scene<C, Ev>>),
-    Pop,
+pub struct DynamicScene<C, Ev>(Arc<AtomicRefCell<dyn Scene<C, Ev>>>);
+
+impl<C, Ev> Clone for DynamicScene<C, Ev> {
+    fn clone(&self) -> Self {
+        Self(self.0.clone())
+    }
+}
+
+impl<C, Ev> fmt::Debug for DynamicScene<C, Ev> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let borrowed = self.0.borrow();
+        let name = borrowed.name();
+        f.debug_tuple("DynamicScene").field(&name).finish()
+    }
+}
+
+impl<C, Ev> DynamicScene<C, Ev> {
+    pub fn new<T>(scene: T) -> Self
+    where
+        T: Scene<C, Ev> + 'static,
+    {
+        Self(Arc::new(AtomicRefCell::new(scene)))
+    }
+
+    fn map_mut_inner<F, R>(&mut self, f: F) -> R
+    where
+        F: FnOnce(&mut dyn Scene<C, Ev>) -> R,
+    {
+        match Arc::get_mut(&mut self.0) {
+            Some(m) => f(m.get_mut()),
+            None => f(&mut *self.0.borrow_mut()),
+        }
+    }
+}
+
+impl<C, Ev> Scene<C, Ev> for DynamicScene<C, Ev> {
+    fn update(&mut self, scene_stack: &mut SceneStack<C, Ev>, ctx: &mut C) -> Result<()> {
+        self.map_mut_inner(|s| s.update(scene_stack, ctx))
+    }
+
+    fn draw(&mut self, ctx: &mut C) -> Result<()> {
+        self.map_mut_inner(|s| s.draw(ctx))
+    }
+
+    fn event(&mut self, ctx: &mut C, event: Ev) {
+        self.map_mut_inner(|s| s.event(ctx, event))
+    }
+
+    fn name(&self) -> Cow<'_, str> {
+        self.0.borrow().name().into_owned().into()
+    }
+
+    fn draw_previous(&self) -> bool {
+        self.0.borrow().draw_previous()
+    }
 }
 
 /// A trait for you to implement on a scene.
 /// Defines the callbacks the scene uses:
 /// a common context type `C`, and an input event type `Ev`.
 pub trait Scene<C, Ev> {
-    fn update(&mut self, ctx: &mut C) -> Result<SceneSwitch<C, Ev>>;
+    fn update(&mut self, scene_stack: &mut SceneStack<C, Ev>, ctx: &mut C) -> Result<()>;
     fn draw(&mut self, ctx: &mut C) -> Result<()>;
     fn event(&mut self, ctx: &mut C, event: Ev);
     /// Only used for human-readable convenience (or not at all, tbh)
-    fn name(&self) -> &str;
+    fn name(&self) -> Cow<'_, str>;
     /// This returns whether or not to draw the next scene down on the
     /// stack as well; this is useful for layers or GUI stuff that
     /// only partially covers the screen.
@@ -69,30 +121,9 @@ pub trait Scene<C, Ev> {
     }
 }
 
-impl<C, Ev> SceneSwitch<C, Ev> {
-    /// Convenient shortcut function for boxing scenes.
-    ///
-    /// Slightly nicer than writing
-    /// `SceneSwitch::Replace(Box::new(x))` all the damn time.
-    pub fn replace<S>(scene: S) -> Self
-    where
-        S: Scene<C, Ev> + 'static,
-    {
-        SceneSwitch::Replace(Box::new(scene))
-    }
-
-    /// Same as `replace()` but returns SceneSwitch::Push
-    pub fn push<S>(scene: S) -> Self
-    where
-        S: Scene<C, Ev> + 'static,
-    {
-        SceneSwitch::Push(Box::new(scene))
-    }
-}
-
 /// A stack of `Scene`'s, together with a context object.
 pub struct SceneStack<C, Ev> {
-    scenes: Vec<Box<dyn Scene<C, Ev>>>,
+    scenes: Vec<DynamicScene<C, Ev>>,
 }
 
 impl<C, Ev> SceneStack<C, Ev> {
@@ -101,72 +132,51 @@ impl<C, Ev> SceneStack<C, Ev> {
     }
 
     /// Add a new scene to the top of the stack.
-    pub fn push(&mut self, scene: Box<dyn Scene<C, Ev>>) {
+    pub fn push(&mut self, scene: DynamicScene<C, Ev>) {
         self.scenes.push(scene)
     }
 
     /// Remove the top scene from the stack and returns it;
     /// panics if there is none.
-    pub fn pop(&mut self) -> Box<dyn Scene<C, Ev>> {
+    pub fn pop(&mut self) -> DynamicScene<C, Ev> {
         self.scenes
             .pop()
             .expect("ERROR: Popped an empty scene stack.")
     }
 
-    /// Returns the current scene; panics if there is none.
-    pub fn current(&self) -> &dyn Scene<C, Ev> {
-        &**self
-            .scenes
-            .last()
-            .expect("ERROR: Tried to get current scene of an empty scene stack.")
+    /// Replace the top scene on the stack by popping and then
+    /// pushing a new scene. Will panic if the stack is empty.
+    /// Returns the replaced scene.
+    pub fn replace(&mut self, scene: DynamicScene<C, Ev>) -> DynamicScene<C, Ev> {
+        let replaced = self.pop();
+        self.push(scene);
+        replaced
     }
 
-    /// Executes the given SceneSwitch command; if it is a pop or replace
-    /// it returns `Some(old_scene)`, otherwise `None`
-    pub fn switch(&mut self, next_scene: SceneSwitch<C, Ev>) -> Option<Box<dyn Scene<C, Ev>>> {
-        match next_scene {
-            SceneSwitch::None => None,
-            SceneSwitch::Pop => {
-                let s = self.pop();
-                log::info!("Pop {}", s.name());
-                Some(s)
-            }
-            SceneSwitch::Push(s) => {
-                log::info!("Push {}", s.name());
-                self.push(s);
-                None
-            }
-            SceneSwitch::Replace(s) => {
-                let old_scene = self.pop();
-                log::info!("Replace {} => {}", old_scene.name(), s.name());
-                self.push(s);
-                Some(old_scene)
-            }
-        }
+    /// Returns the current scene; panics if there is none.
+    pub fn current(&self) -> &DynamicScene<C, Ev> {
+        self.scenes
+            .last()
+            .expect("ERROR: Tried to get current scene of an empty scene stack.")
     }
 
     // These functions must be on the SceneStack because otherwise
     // if you try to get the current scene and the world to call
     // update() on the current scene it causes a double-borrow.  :/
     pub fn update(&mut self, ctx: &mut C) -> Result<()> {
-        let next_scene = {
-            let current_scene = &mut **self
-                .scenes
-                .last_mut()
-                .expect("Tried to update empty scene stack");
-            current_scene.update(ctx)?
-        };
-
-        self.switch(next_scene);
-
-        Ok(())
+        let mut current_scene = self
+            .scenes
+            .last()
+            .cloned()
+            .expect("Tried to update empty scene stack");
+        current_scene.update(self, ctx)
     }
 
     /// We walk down the scene stack until we find a scene where we aren't
     /// supposed to draw the previous one, then draw them from the bottom up.
     ///
     /// This allows for layering GUI's and such.
-    fn draw_scenes(scenes: &mut [Box<dyn Scene<C, Ev>>], ctx: &mut C) -> Result<()> {
+    fn draw_scenes(scenes: &mut [DynamicScene<C, Ev>], ctx: &mut C) -> Result<()> {
         assert!(scenes.len() > 0);
         if let Some((current, rest)) = scenes.split_last_mut() {
             if current.draw_previous() {
@@ -185,7 +195,7 @@ impl<C, Ev> SceneStack<C, Ev> {
 
     /// Feeds the given event to the current scene.
     pub fn event(&mut self, ctx: &mut C, event: Ev) {
-        let current_scene = &mut **self
+        let current_scene = self
             .scenes
             .last_mut()
             .expect("Tried to do input for empty scene stack");
